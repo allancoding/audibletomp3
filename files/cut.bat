2d6ec8817922554c7938665e7f525c08c60deabe
alniles <74841470+alniles@users.noreply.github.com>
2021-03-09 18:43:37 -0700

    Add files via upload

---
+++ files/cut.bat
@@ -0,0 +1,18 @@
+#!/bin/bat -efu
+
+input="../files/Your Book!.mp3"
+ffprobe \
+    -print_format csv \
+    -show_chapters \
+    "$input" |
+cut -d ',' -f '5,7,8' |
+while IFS=, read start end chapter
+do
+    ffmpeg \
+        -nostdin \
+        -ss "$start" -to "$end" \
+        -i "$input" \
+        -c copy \
+        -map 0 \
+        "${input%.*}-$chapter.${input##*.}"
+done
